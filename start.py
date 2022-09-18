c7278b725c21854dba02bcef6e87aad56c18f666
ldco <ldco@pop-os.localdomain>
2022-09-18 09:16:04 +0300

    screen size fix

--- start.py
+++ start.py
@@ -1,16 +1,16 @@
 import PySimpleGUIQt as sg
 
 _window = sg.Window('')
-windowHeight = _window.get_screen_dimensions()[1]
-windowWidth = _window.get_screen_dimensions()[0]/2
+displayHeight = _window.get_screen_dimensions()[1]
+displayWidth = _window.get_screen_dimensions()[0]
 
 uifont = "PTSans"
 sz = (10,20)
 
 def move_center(window):
     screen_width, screen_height = window.get_screen_dimensions()
-    # win_width, win_height = window.size
-    x, y = (screen_width - windowHeight)/2, (screen_height - windowHeight)/2
+    win_width, win_height = window.size
+    x, y = (screen_width - win_width)/2, (screen_height - win_height)
     window.move(x, y)
 
 # Add your new theme colors and settings
@@ -20,7 +20,7 @@ sg.LOOK_AND_FEEL_TABLE['f21theme'] = {'BACKGROUND': '#303030', 'TEXT': '#efefef'
 # Switch to use your newly created theme
 sg.theme('f21theme')
 # sg.theme('SystemDefaultForReal')
-sg.set_options(font=(uifont, 15))
+sg.set_options(font=(uifont, 12))
 
 col1 = [
             [sg.HorizontalSeparator()],
@@ -44,8 +44,10 @@ col1 = [
             [sg.Text("Compress with PNGQUANT", font=(uifont, 9))],
             [sg.HorizontalSeparator()],
             [sg.Checkbox('Compress', default=True)],
+            [sg.Radio('Rewrite files', "RADIO3", default=True), sg.Radio('New files', "RADIO3")],
             [sg.HorizontalSeparator()],
-            [sg.Button('OK', size=(windowWidth/3, 80)), sg.Button('CANCEL', size=(windowWidth/3, 80))]
+            [sg.HorizontalSeparator()],
+            [sg.Button('OK', size=(displayWidth/3, 80)), sg.Button('CANCEL', size=(displayWidth/3, 80))]
             ]
 col2 = [
             [sg.HorizontalSeparator()],
@@ -62,11 +64,11 @@ layout = [
     ]
 
 # Create the Window
-window = sg.Window('Image Fabrica', layout, size=(windowHeight, windowHeight), finalize=True)
+window = sg.Window('Image Fabrica', layout, size=(displayWidth/2, displayHeight), finalize=True)
 
 # Event Loop to process "events" and get the "values" of the inputs
 move_center(window)
-_window.close()
+# _window.close()
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
